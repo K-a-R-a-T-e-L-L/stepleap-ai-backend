ab5265de7d7e30de88d72ce0fb4d8fed601e8bac
Stepan Palamarchuk <webocono@gmail.com>
2025-12-02 14:34:19 +0300

    GitLab CI

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, configSwagger)
     SwaggerModule.setup('api-docs', app, document)
 
-    await app.listen(3000)
+    await app.listen(3200)
 }
 
 bootstrap()
