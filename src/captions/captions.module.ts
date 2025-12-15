4541b42611bb40b74aadf9825f980ca00cff5fe5
Stepan Palamarchuk <webocono@gmail.com>
2025-12-15 16:20:04 +0300

    Fix module import

--- src/captions/captions.module.ts
+++ src/captions/captions.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 import { CaptionsController } from './controllers/captions.controller'
 import { CaptionsService } from './services/captions.service'
 import { FileModule } from '../file/file.module'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-    imports: [FileModule],
+    imports: [FileModule, JwtModule],
     controllers: [CaptionsController],
-    providers: [CaptionsService]
+    providers: [CaptionsService],
 })
 export class CaptionsModule {}
