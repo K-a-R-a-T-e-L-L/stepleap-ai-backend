1ce33a899460a3dd5bd5ae52420cfc7fae5195c0
Stepan Palamarchuk <webocono@gmail.com>
2025-12-15 16:45:15 +0300

    Fix swagger definitions

--- src/captions/dto/create-video.dto.ts
+++ src/captions/dto/create-video.dto.ts
@@ -1,4 +1,9 @@
+import { TypeValidate, Validate } from '@common/decorators/validation.helpers'
+
 export class CreateVideoDto {
+    @Validate(TypeValidate.STRING)
     imageReference: string
+
+    @Validate(TypeValidate.STRING)
     audioReference: string
 }
