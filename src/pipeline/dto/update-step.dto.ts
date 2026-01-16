dd2119f57dce64482b560d34f8fb8cf4cd79e21e
Stepan Palamarchuk <webocono@gmail.com>
2026-01-16 13:50:35 +0300

    Fix import

--- src/pipeline/dto/update-step.dto.ts
+++ src/pipeline/dto/update-step.dto.ts
@@ -1,4 +1,4 @@
 import { CreateStepDto } from './create-step.dto'
-import { PartialType } from '@nestjs/mapped-types'
+import { PartialType } from '@nestjs/swagger'
 
 export class UpdateStepDto extends PartialType(CreateStepDto) {}
